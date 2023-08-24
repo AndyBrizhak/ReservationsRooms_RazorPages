41e4a8c6ea8a5c2363f6365691bf5f43860ff2e0
Андрей Брижак <andrewbrizhak@gmail.com>
2023-08-25 06:48:26 +0800

    Add attribute BindProperties on Index page

--- ReservationsRooms_RazorPages/Pages/Index.cshtml.cs
+++ ReservationsRooms_RazorPages/Pages/Index.cshtml.cs
@@ -7,11 +7,12 @@ using System.Dynamic;
 
 namespace ReservationsRooms_RazorPages.Pages
 {
+    [BindProperties]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
-        [BindProperty]
+        //[BindProperty]
         public List<Room> Rooms { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger )
@@ -23,32 +24,38 @@ namespace ReservationsRooms_RazorPages.Pages
 
         //public List<Room> Rooms { get; set; } = new List<Room>();
 
-        public  void OnGet()
+        public   void OnGet()
         {
             Rooms=SD.Rooms;
-            //return Content("Test"); 
+            //return RedirectToPage("Index"); 
 
         }
 
-        public void OnPut(int id, string status)
-        {
-            try
-            {
-                var room = Rooms.FirstOrDefault(r => r.Id == id);
-                room.Status = status;
-            }
-            catch
-            {
-                //Exception ex;
-                _logger.Log(
-                    LogLevel.Warning,
-                    "Not valid Id");
+        //public IActionResult OnPost(Room room)
+        //{
+        //    Rooms.Add(room);
+        //    return  RedirectToPage("Index");
+        //}
 
-            }
+        //public void OnPut(int id, string status)
+        //{
+        //    try
+        //    {
+        //        var room = Rooms.FirstOrDefault(r => r.Id == id);
+        //        room.Status = status;
+        //    }
+        //    catch
+        //    {
+        //        //Exception ex;
+        //        _logger.Log(
+        //            LogLevel.Warning,
+        //            "Not valid Id");
+
+        //    }
 
 
 
-        }
+        //}
     }
 
 
