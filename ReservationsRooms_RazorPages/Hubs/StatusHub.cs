26b8652b3811cd78ccb25839ec43518a2dd4a63a
Андрей Брижак <andrewbrizhak@gmail.com>
2023-08-27 14:20:29 +0800

    change ui rooms page

--- ReservationsRooms_RazorPages/Hubs/StatusHub.cs
+++ ReservationsRooms_RazorPages/Hubs/StatusHub.cs
@@ -7,11 +7,11 @@ namespace Reservations.Hubs
 		//public int RoomId { get; set; }
   //      public string StatusRoom { get; set; }
 
-		public async Task NewStatusRoomLoaded(int id,	string status)
+		public async Task NewStatusRoomLoaded(int id, string currentStatus,	string newStatus)
 		{
-			SD.Rooms[id].Status=status;
+			SD.Rooms[id].Status=newStatus;
 			//send update client, that status room have been updated
-			await Clients.Caller.SendAsync("changeStatus", id, status);
+			await Clients.Caller.SendAsync("changeStatus", id, currentStatus, newStatus);
 		}
     }
 }
--- ReservationsRooms_RazorPages/wwwroot/js/updateStatusRoom.js
+++ ReservationsRooms_RazorPages/wwwroot/js/updateStatusRoom.js
@@ -1,21 +1,20 @@
-﻿const id;
-const status;
-
+﻿
 // create connection
 var connectionStatusHub = new signalR.HubConnectionBuilder()
     .withUrl("/hubs/statushub")
     .Build();
 
 //connect to method that hub invokes, aka receive shange status from hub
-connectionStatusHub.on("changeStatus", (id, status) =>{
-    var newStatus = document.getElementById(newStatusId);
-    newStatus.innerText = status.toString();
+connectionStatusHub.on("changeStatus", (id, currentStatus, newStatus) =>{
+    /*const currentIdStatus = id + status;*/
+    const currentIdStatus = document.getElementById(id+currentStatus);
+    currentIdStatus.innerText = newStatus.toString();
 })
 
 // invoke hub method, aka send notification to hub
 function NewStatusRoomLoadedOnClient() {
-    var status = document.getElementById(currentStatus);
-    var id = document.getElementById(currentId);
+    //var status = document.getElementById(currentStatus);
+    //var id = document.getElementById(currentId);
     connectionStatusHub.send("NewStatusRoomLoaded", id, status)
 }
 
