25766b47e819f8b8d02cf89ed8fcc849d18b27cf
Андрей Брижак <andrewbrizhak@gmail.com>
2023-08-27 09:25:45 +0800

    Add method to Hub, invoke method in client to update status

--- ReservationsRooms_RazorPages/Hubs/StatusHub.cs
+++ ReservationsRooms_RazorPages/Hubs/StatusHub.cs
@@ -4,5 +4,14 @@ namespace Reservations.Hubs
 {
 	public class StatusHub: Hub
 	{
-	}
+		public int RoomId { get; set; }
+        public string StatusRoom { get; set; }
+
+		public async	Task	NewStatusRoomLoaded(int id,	string status)
+		{
+			SD.Rooms[id].Status=status;
+			//send update client, that status room have been updated
+			await Clients.Caller.SendAsync("updateStatusRoom", id, status);
+		}
+    }
 }
