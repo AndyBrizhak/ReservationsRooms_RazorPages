66714088f215c4f931f420205e732fdd013d0b8f
Андрей Брижак <andrewbrizhak@gmail.com>
2023-08-27 10:34:07 +0800

    add to signalr hub from client js and start configure receive update from hub and perorms action

--- ReservationsRooms_RazorPages/Hubs/StatusHub.cs
+++ ReservationsRooms_RazorPages/Hubs/StatusHub.cs
@@ -4,14 +4,14 @@ namespace Reservations.Hubs
 {
 	public class StatusHub: Hub
 	{
-		public int RoomId { get; set; }
-        public string StatusRoom { get; set; }
+		//public int RoomId { get; set; }
+  //      public string StatusRoom { get; set; }
 
 		public async	Task	NewStatusRoomLoaded(int id,	string status)
 		{
 			SD.Rooms[id].Status=status;
 			//send update client, that status room have been updated
-			await Clients.Caller.SendAsync("updateStatusRoom", id, status);
+			await Clients.Caller.SendAsync("changeStatus", id, status);
 		}
     }
 }
---
+++ ReservationsRooms_RazorPages/wwwroot/js/updateStatusRoom.js
@@ -0,0 +1,30 @@
+﻿const id;
+const status;
+
+// create connection
+var connectionStatusHub = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/statushub")
+    .Build();
+
+//connect to method that hub invokes, aka receive shange status from hub
+connectionStatusHub.on("changeStatus", (id, status) =>{
+    var newStatus = document.getElementById(newStatusId);
+    newStatus.innerText = status.toString();
+})
+
+// invoke hub method, aka send notification to hub
+function NewStatusRoomLoadedOnClient() {
+    connectionStatusHub.send("NewStatusRoomLoaded", id, status)
+}
+
+//start connection
+function fullfilled() {
+// do something on start
+console.log("Connection to Status Hub Successful")
+}
+
+function rejected() {
+//rejected logs
+}
+
+connectionStatusHub.start().then(fullfilled, rejected);
