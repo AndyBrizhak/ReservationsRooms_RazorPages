698f2012d30300370e5a7e4f60168f434cfa5e4c
Андрей Брижак <andrewbrizhak@gmail.com>
2023-08-27 18:57:46 +0800

    try to fix bug with signalr

--- ReservationsRooms_RazorPages/wwwroot/js/updateStatusRoom.js
+++ ReservationsRooms_RazorPages/wwwroot/js/updateStatusRoom.js
@@ -1,29 +1,36 @@
-﻿const btn_currentClick;
-const btn_currentClickId
+﻿//const btn_currentClick;
+//const btn_currentClickId
 
 
+
+const btn_1_ToAvailable = document.getElementById("1:available"); 
 
 // create connection
 var connectionStatusHub = new signalR.HubConnectionBuilder()
     .withUrl("/hubs/statushub")
     .Build();
 
-btn_currentClickId= document.addEventListener("click", function (event) {
-    return event.target.id;
+btn_1_ToAvailable.addEventListener("click", function (e) {
+    connectionStatusHub.send("NewStatusRoomLoaded", "1:available");
+    e.preventDefault();
 });
+//btn_currentClickId= document.addEventListener("click", function (event) {
+//    return event.target.id;
+//});
 
-btn_currentClick = document.getElementById(btn_currentClickId);
+//btn_currentClick = document.getElementById(btn_currentClickId);
 
-btn_currentClick.addEventListener("click"), function (event) {
-    connectionStatusHub.send("NewStatusRoomLoaded", btn_currentClickId)
-}
+//btn_currentClick.addEventListener("click"), function (event) {
+//    connectionStatusHub.send("NewStatusRoomLoaded", btn_currentClickId)
+//}
 
 
 //connect to method that hub invokes, aka receive shange status from hub
-connectionStatusHub.on("changeStatus", (id, newStatus) =>{
+connectionStatusHub.on("changeStatus", (idStatus) => {
+    btn_1_ToAvailable.innerText = idStatus;
     /*const currentIdStatus = id + status;*/
-    const currentIdStatus = document.getElementById(id+currentStatus);
-    currentIdStatus.innerText = newStatus.toString();
+    //const currentIdStatus = document.getElementById(id+currentStatus);
+    //currentIdStatus.innerText = newStatus.toString();
     /*btn_currentClick = document.getElementById(id+currentStatus+newStatus);*/
 
 })
@@ -43,10 +50,6 @@ console.log("Connection to Status Hub Successful")
 
 function rejected() {
 //rejected logs
-}
-
-function myfunction() {
-
 }
 
 connectionStatusHub.start().then(fullfilled, rejected);
