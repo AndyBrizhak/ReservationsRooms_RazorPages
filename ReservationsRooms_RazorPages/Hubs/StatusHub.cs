0e01623a1e3c855de1791c1ee69634a10a5e0ed9
Андрей Брижак <andrewbrizhak@gmail.com>
2023-09-11 08:59:17 +0800

    Add define  dynamic generation Id for button

--- ReservationsRooms_RazorPages/Hubs/StatusHub.cs
+++ ReservationsRooms_RazorPages/Hubs/StatusHub.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Reservations.Models;
 
 namespace Reservations.Hubs
 {
 	public class StatusHub: Hub
 	{
 		public int RoomId { get; set; }
-		public string StatusRoom { get; set; }
+		public string? StatusRoom { get; set; }
+		public string? BtnStatusId { get; set; }
 
 		public async Task NewStatus(int idRoom, string newStatus)
         {
 			SD.Rooms[(idRoom-1)].Status = newStatus;
+			SD.Rooms[(idRoom-1)].BtnStatusId = newStatus;
 			RoomId = idRoom;
 			StatusRoom = newStatus;
+			BtnStatusId = $"{newStatus}{idRoom}";
 
 			//send update all client, that status room have been updated
-			await Clients.All.SendAsync("changeStatus", StatusRoom, RoomId);
+			await Clients.All.SendAsync("changeStatus", StatusRoom, RoomId, BtnStatusId);
 		}
     }
 }
