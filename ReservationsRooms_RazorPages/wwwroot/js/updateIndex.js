d68a95c0907a38c9080da38fb1bd1829db9e2d0f
Андрей Брижак <andrewbrizhak@gmail.com>
2023-09-26 05:12:41 +0800

    Add script and hook for Index page

--- ReservationsRooms_RazorPages/wwwroot/js/updateIndex.js
+++ ReservationsRooms_RazorPages/wwwroot/js/updateIndex.js
@@ -4,13 +4,13 @@ var connectionStatusHub = new signalR.HubConnectionBuilder()
     .withUrl("/hubs/statushub")
     .Build();
 
-btn_1_ToAvailable.addEventListener("click", function (e) {
-    connectionStatusHub.send("NewStatusRoomLoaded", "1:available");
-    e.preventDefault();
-});
+//btn_1_ToAvailable.addEventListener("click", function (e) {
+//    connectionStatusHub.send("NewStatusRoomLoaded", "1:available");
+//    e.preventDefault();
+//});
 
 //connect to method that hub invokes, aka receive shange status from hub
-connectionStatusHub.on("changeStatus", function (status, id) {
+connectionStatusHub.on("updateRoomOnIndex", function (status, id) {
     var lblCurrent = document.getElementById("btnCurrent_" + id);
     if (typeof (lblCurrent) != 'undefined' && lblCurrent != null) {
         // Exists
